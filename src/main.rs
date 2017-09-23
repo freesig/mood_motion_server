cd38736093ebaf419b813c69117ec6a330be9768
Tom Gowan <s3549186@student.rmit.edu.au>
2017-09-23 13:03:20 +1000

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,116 @@
+#[macro_use]
+extern crate glium;
+
+use std::net::UdpSocket;
+use std::ops::{Sub, AddAssign, DivAssign};
+use glium::{glutin, Surface};
+
+#[derive(Debug)]
+struct Vec3{
+    x: f32,
+    y: f32,
+    z: f32,
+}
+
+impl<'a, 'b> Sub<&'b Vec3> for &'a Vec3{
+    type Output = Vec3;
+
+    fn sub(self, other: &'b Vec3) -> Vec3 {
+        Vec3{ x: self.x - other.x,
+        y: self.y - other.y,
+        z: self.z - other.z
+        }
+    }
+}
+
+impl AddAssign for Vec3{
+    fn add_assign(&mut self, other: Vec3) {
+        *self = Vec3{
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        };
+    }
+}
+
+impl Vec3{
+    fn scale(&mut self, amount: f32) {
+        *self = Vec3{
+            x: self.x * amount,
+            y: self.y * amount,
+            z: self.z * amount,
+        };
+    }
+}
+
+fn max(l: f32, r: f32) -> f32{
+    if l.ge(&r) {
+        l
+    }else{
+        r
+    }
+}
+
+fn min(l: f32, r: f32) -> f32{
+    if l.le(&r) {
+        l
+    }else{
+        r
+    }
+}
+
+fn main() {
+    use glium::{glutin, Surface};
+
+    let mut events_loop = glutin::EventsLoop::new();
+    let window = glutin::WindowBuilder::new();
+    let context = glutin::ContextBuilder::new();
+    let display = glium::Display::new(window, context, &events_loop).unwrap();
+
+    let mut socket = UdpSocket::bind("0.0.0.0:44444").unwrap();
+
+    let mut closed = false;
+    // read from the socket
+    let mut buf = [0; 100];
+    let mut history: Vec<Vec3> = Vec::new();
+    while !closed {
+        let (amt, src) = socket.recv_from(&mut buf).unwrap();
+
+        let data = std::str::from_utf8(&buf[..]).unwrap();
+        let mut peices = data.split(",");
+        let x: f32 = peices.next().unwrap().parse().unwrap();
+        let y: f32 = peices.next().unwrap().parse().unwrap();
+        let z: f32 = peices.next().unwrap().parse().unwrap();
+        let accel = Vec3{x, y, z};
+
+        history.push(accel);
+        if history.len() >= 10 {
+            history.remove(0);
+        }
+
+        let mut dj_total = Vec3{ x: 0.0, y: 0.0, z: 0.0 };
+        for i in 0..(history.len() - 1) {
+            dj_total += &history[i+1] - &history[i];
+
+        }
+        dj_total.scale(1.0 / history.len() as f32);
+        println!("dj_total: {:?}", dj_total);
+
+        dj_total.x = max( min( dj_total.x.abs(), 1.0 ), 0.0);
+        dj_total.y = max( min( dj_total.y.abs(), 1.0 ), 0.0);
+        dj_total.z = max( min( dj_total.z.abs(), 1.0 ), 0.0);
+        let mut target = display.draw();
+        target.clear_color(dj_total.x.abs(), dj_total.y.abs(), dj_total.z.abs(), 1.0);
+        target.finish().unwrap();
+
+        events_loop.poll_events(|event| {
+            match event {
+                glutin::Event::WindowEvent { event, .. } => match event {
+                    glutin::WindowEvent::Closed => closed = true,
+                    _ => ()
+                },
+                _ => (),
+            }
+        });
+    }
+}
---
+++ target/debug/mood_motion_server.d
@@ -0,0 +1,1 @@
+/Users/tomgowan/Documents/Interactive/mood_motion_server/target/debug/mood_motion_server: /Users/tomgowan/Documents/Interactive/mood_motion_server/src/main.rs
